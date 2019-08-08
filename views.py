240ca73a8e1bdbc963d8fc91530aa5001c88be23
Benjamin Schmid <bene.schmid@gmail.com>
2019-08-08 14:27:24 +0000

    Download and store image locally.

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 import tempfile
 import os
 import datetime
+import shutil
+from pytiff import PyTiff
 
 FIJI_DIR = "/usr/local/share/Fiji.app/"
 FIJI_BIN = FIJI_DIR + "ImageJ-linux64"
@@ -25,7 +27,6 @@ def index(request, conn=None, **kwargs):
            {'imageId': image_id, 'image_name': image_name,
             'z_indexes': z_indexes})
 
-
 @login_required()
 def render3D(request, conn=None, **kwargs):
      """ Shows a subset of Z-planes for an image """
@@ -37,8 +38,9 @@ def render3D(request, conn=None, **kwargs):
      size_z = image.getSizeZ()
      z_indexes = [0, int(size_z*0.25), int(size_z*0.5),
           int(size_z*0.75), size_z-1]
+     imagepath = extract_image(conn, image)
      s = request.GET['script']
-     macrofile = makeMacro(s, basename)
+     macrofile = makeMacro(s, basename, imagepath, image.getSizeC(), image.getSizeZ(), image.getSizeT())
      avifile = basename + ".avi"
      animationfile = basename + ".animation.txt"
      run3Dscript(macrofile, avifile)
@@ -59,8 +61,44 @@ def render3D(request, conn=None, **kwargs):
      os.remove(macrofile)
      # aId = 107 # for now put manually, because we need an mp4
      return render(request, '3Dscript/index.html',
-           {'imageId': image_id, 'image_name': "bla",
+           {'imageId': image_id, 'image_name': image_name,
             'z_indexes': z_indexes, 's': s, 'annotationId' : aId, 'macro' : macrofile, 'outfile' : mp4file})
+
+def extract_image(conn, image):
+     image_id = image.getId()
+     tmp_folder = '/tmp/3Dscript.%s/' % (image_id)
+     if os.path.exists(tmp_folder):
+          shutil.rmtree(tmp_folder)
+     os.mkdir(tmp_folder)
+     w = image.getSizeX()
+     h = image.getSizeY()
+     channels = image.getSizeC()
+     slices = image.getSizeZ()
+     frames = image.getSizeT()
+     size_x_obj = image.getPixelSizeX(units=True)
+     size_y_obj = image.getPixelSizeY(units=True)
+     size_z_obj = image.getPixelSizeZ(units=True)
+     pw = size_x_obj.getValue()
+     ph = size_y_obj.getValue()
+     pd = size_z_obj.getValue()
+     units = size_x_obj.getSymbol()
+     units = units.replace('\xc2\xb5', 'u') # microns
+     pixels = image.getPrimaryPixels()
+     dtype = image.getPixelsType() # either 'uint8' or 'uint16'
+     # TODO raise error if not 'uint8' or 'uint16'
+     is16 = dtype == 'uint16'
+     tiffHdr = PyTiff.get_tiff_header(w, h, channels, slices, frames, pw, ph, pd, units, is16)
+     for t in range(frames):
+          impath = "%s/%04d.tif" % (tmp_folder, t)
+          fid = open(impath, "wb")
+          fid.write(tiffHdr)
+          for z in range(slices):
+               for c in range(channels):
+                    plane = pixels.getPlane(z, c, t)
+                    fid.write(plane.tobytes())
+          fid.close()
+
+     return tmp_folder
 
 def convertToMP4(avifile):
      from subprocess import Popen, PIPE
@@ -76,7 +114,7 @@ def convertToMP4(avifile):
 
 def run3Dscript(macrofile, outfile):
      from subprocess import Popen, PIPE
-     cmd = [FIJI_BIN, '--headless', '--console', '-macro', macrofile, outfile]
+     cmd = [FIJI_BIN, '--console', '-macro', macrofile, outfile]
      out = open("/tmp/fijiout.txt", "w")
      err = open("/tmp/fijierr.txt", "w")
      p = Popen(cmd, stdout=out, stderr=err)
@@ -84,7 +122,7 @@ def run3Dscript(macrofile, outfile):
      out.close()
      err.close()
 
-def makeMacro(script, basename):
+def makeMacro(script, basename, path, nChannels, nSlices, nFrames):
      s = '''
 outfile = "''' + basename + '''.avi";
 setBatchMode(true);
@@ -102,8 +140,10 @@ function getAnimationFileName() {
 	return "''' + basename + '''.animation.txt";
 }
 
-run("T1 Head (2.4M, 16-bits)");
-// open("/Users/bene/t1-head.tif");
+// run("T1 Head (2.4M, 16-bits)");
+// open("''' + path + '''");
+run("Image Sequence...", "open=[''' + path + '''] file=tif sort use");
+run("Stack to Hyperstack...", "order=xyczt(default) channels=''' + str(nChannels) + ''' slices=''' + str(nSlices) + ''' frames=''' + str(nFrames) + ''' display=Composite");
 title = getTitle();
 
 
