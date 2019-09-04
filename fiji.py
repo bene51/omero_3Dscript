3a09d37715aa5c076d3fbdb360a0a777c05c273e
Benjamin Schmid <bene.schmid@gmail.com>
2019-09-04 09:14:32 +0000

    More WIP.

--- fiji.py
+++ fiji.py
@@ -1,20 +1,33 @@
 import sys
 import socket
 import time
+import threading
 from subprocess import Popen, PIPE
 
 FIJI_DIR = "/usr/local/share/Fiji.app/"
 FIJI_BIN = FIJI_DIR + "ImageJ-linux64"
 
-def startFiji():
+
+def startFiji(co):
 	print("startFiji")
-	cmd = [FIJI_BIN, '--console', '--headless', '-eval', 'run("3Dscript Server", "");']
-	print(cmd[4])
-	p = Popen(cmd, stdout=PIPE)
-	for line in iter(p.stdout.readline, b''):
-		print(line)
-		if line.startswith('Waiting for connection...'):
-			break;
+	stdoutput = ""
+	try:
+		log = open("/tmp/fiji.out", 'w')
+		err = open("/tmp/fiji.err", 'w')
+		cmd = [FIJI_BIN, '--console', '--headless', '-eval', 'run("3Dscript Server", "");']
+		print(cmd[4])
+		p = Popen(cmd, stdout=PIPE, stderr=err)
+		for line in iter(p.stdout.readline, b''):
+			stdoutput = stdoutput + line
+			log.write(line)
+			log.flush()
+			if line.startswith('Waiting for connection...'):
+				co.set()
+				#with co:
+				#	co.notifyAll()
+	finally:
+		log.close()
+		err.close()
     
 
 def send(msg):
@@ -23,7 +36,13 @@ def send(msg):
 		s.connect(('localhost', 3333))
 	except socket.error as e:
 		if e.errno == 111:
-			startFiji()
+			co = threading.Event()
+			threading.Thread(name='startFiji', target=startFiji, args=(co,)).start()
+			# startFiji(co)
+			# with co:
+			co.wait(15.0) #seconds
+			if not co.is_set(): #timeout
+				raise Exception("Unable to start Fiji"); #TODO kill fiji
 			return send(msg)
 	s.sendall(msg)
 	data = None
@@ -50,7 +69,8 @@ def run(host, sessionid, basename, imageid, w, h):
 			break
 		time.sleep(0.1)
 
-host = 'omero'
-sessionid = '9et1v5f8ftzswwqwnkwh5a7630bknk4x'
-basename = '/tmp/bschmid-2019-08-23-15-43-24'
-run(host, sessionid, basename, 1, 256, 256)
+if False:
+	host = 'omero'
+	sessionid = '9et1v5f8ftzswwqwnkwh5a7630bknk4x'
+	basename = '/tmp/bschmid-2019-08-23-15-43-24'
+	run(host, sessionid, basename, 1, 256, 256)
