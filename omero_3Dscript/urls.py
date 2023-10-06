556f4361be229451761c54b52e4f2be7deb2fc0b
Benjamin Schmid <bene.schmid@gmail.com>
2023-10-06 15:53:57 +0200

    Update for new Django version.

--- omero_3Dscript/urls.py
+++ omero_3Dscript/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='3Dscript_index'),
-	url(r'^startRendering$', views.startRendering, name='3Dscript_startRendering'),
-	url(r'^getImages$', views.getImages, name='3Dscript_getImages'),
-	url(r'^getStateAndProgress$', views.getStateAndProgress, name='3Dscript_getStateAndProgress'),
-	url(r'^cancelRendering$', views.cancelRendering, name='3Dscript_cancelRendering'),
-	url(r'^getVideo$', views.getVideo, name='3Dscript_getVideo'),
+	re_path(r'^$', views.index, name='3Dscript_index'),
+	re_path(r'^startRendering$', views.startRendering, name='3Dscript_startRendering'),
+	re_path(r'^getImages$', views.getImages, name='3Dscript_getImages'),
+	re_path(r'^getStateAndProgress$', views.getStateAndProgress, name='3Dscript_getStateAndProgress'),
+	re_path(r'^cancelRendering$', views.cancelRendering, name='3Dscript_cancelRendering'),
+	re_path(r'^getVideo$', views.getVideo, name='3Dscript_getVideo'),
 ]
